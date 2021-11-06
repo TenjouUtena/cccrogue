09b09b3eca9aa227dca25194afd32678d1442c6f
Brendan S <brendan.speer@gmail.com>
2021-11-06 15:03:47 -0500

    *setup mk. 2

--- cccr-frontend/src/App.js
+++ cccr-frontend/src/App.js
@@ -1,22 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+class Hello extends React.Component {
+  state = {
+    message: "Cannot connect to backend."
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:5000/")
+    .then(res => res.json())
+    .then((result) => {
+      this.setState({message:result})
+    })
+  };
+
+  render() {
+    return (
+      <div>
+      <span>Hello World</span>
+      <p />
+      <span>{this.state.message}</span>
+      </div>
+      )
+  };
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hello />
     </div>
   );
 }
